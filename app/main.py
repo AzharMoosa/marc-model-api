010d7d78b8cfcc59f1df2bbcb16037fdd595bf2a
AzharMoosa <azhar.moosa8@gmail.com>
2023-05-08 10:54:58 +0100

    Added Better Calculator Formatter

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-# from fastT5 import get_onnx_model, get_onnx_runtime_sessions, OnnxT5
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from pathlib import Path
 import os
 from fastapi import FastAPI
 from pydantic import BaseModel
+import re
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
@@ -18,30 +18,53 @@ class QuestionResponse(BaseModel):
     answer: str
 
 
-path_to_model = "./marc"
-model_name = Path(path_to_model).stem
+path_to_model = f"{__location__}/model"
+path_to_tokenizer = f"{__location__}/tokenizer"
 
-path_to_encoder = os.path.join(path_to_model, f"{model_name}-encoder-quantized.onnx")
-path_to_decoder = os.path.join(path_to_model, f"{model_name}-decoder-quantized.onnx")
-path_to_init_decoder = os.path.join(
-    path_to_model, f"{model_name}-init-decoder-quantized.onnx"
-)
-
-# model_paths = path_to_encoder, path_to_decoder, path_to_init_decoder
-# model_sessions = get_onnx_runtime_sessions(model_paths)
-# model = OnnxT5(path_to_model, model_sessions)
-# tokenizer = AutoTokenizer.from_pretrained(path_to_model)
-
-path_to_model = f"{__location__}/model_and_tokenizer/model"
-path_to_tokenizer = f"{__location__}/model_and_tokenizer/tokenizer"
 tokenizer = AutoTokenizer.from_pretrained(path_to_tokenizer)
 model = AutoModelForSeq2SeqLM.from_pretrained(path_to_model)
 
 
-def answer_question(question, m, t):
-    encoded_text = t.encode(question, return_tensors="pt")
-    model_output = m.generate(encoded_text, do_sample=True, top_p=0.9, max_length=512)
-    answer = t.decode(model_output[0], skip_special_tokens=True)
+def clean_up_calculations(text):
+    CALCULATOR_PATTERN = r"\d+[+/*x-]\d+=\d+>>\d+"
+    FINAL_ANSWER_NUMBER_PATTERN = r"\d+\S*$"
+    NO_SPACE_EQUATION_PATTERN = r"(\d+)([+\-*/])(\d+)"
+
+    # Find All Calculator Notions Of Form "X+Y=Z>>Z"
+    calculator_notation = re.findall(CALCULATOR_PATTERN, text)
+
+    if not calculator_notation:
+        return text
+
+    correct_answer = None
+
+    for notation in calculator_notation:
+        # Replace "X+Y=Z>>Z" To Evaluated Value "X+Y"
+        expression = notation.split(">>")
+        equation = expression[0].strip()
+        lhs = equation.split("=")
+        if not lhs:
+            continue
+        correct_answer = eval(lhs[0])
+        text = text.replace(notation, str(correct_answer))
+
+    # Verify Final Answer Is Same As Final Calculation
+    if correct_answer and re.search(FINAL_ANSWER_NUMBER_PATTERN, text):
+        text = re.sub(FINAL_ANSWER_NUMBER_PATTERN, str(correct_answer), text)
+
+    # Add Space Between Equations: "X+Y=Z" => "X + Y = Z"
+    text = re.sub(NO_SPACE_EQUATION_PATTERN, r"\1 \2 \3", text)
+
+    return text
+
+
+def answer_question(question):
+    encoded_text = tokenizer.encode(question, return_tensors="pt")
+    model_output = model.generate(
+        encoded_text, do_sample=True, top_p=0.9, max_length=256
+    )
+    answer = tokenizer.decode(model_output[0], skip_special_tokens=True)
+    answer = clean_up_calculations(answer)
     return answer
 
 
@@ -54,7 +77,7 @@ def index():
 def solve_math_problem(request: QuestionRequest):
     try:
         question = request.question
-        answer = answer_question(question, model, tokenizer)
+        answer = answer_question(question)
         return QuestionResponse(answer=answer)
     except:
         return QuestionResponse(answer="SERVER ERROR")
